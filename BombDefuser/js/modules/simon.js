9582c4e7f9fb18cc0050a1a6d9b97d791b3fe583
Night5word <afga93+git@gmail.com>
2016-09-28 14:29:39 +0200

    added simon module

---
+++ BombDefuser/js/modules/simon.js
@@ -0,0 +1,26 @@
+modules.push({
+	getMenuEntry : function(){ 
+		return {
+			name : "Simon",
+			img : "SimonComponent.svg"
+		};
+	},
+
+	getSolver : function(){
+		function init(outputDOMElems){
+			
+		}
+
+		function onchange(data, outputDOMElems){
+			
+		}
+
+		return {
+			fields : [
+
+			],
+			init : init,
+			onchange : onchange
+		}
+	}
+});
