d6b238e5262656043215b9014e4b4a2af5f717f5
Bundas102 <robert.ban102@gmail.com>
2017-07-17 19:46:50 +0200

    button cleanup

--- BombDefuser/js/modules/button.js
+++ BombDefuser/js/modules/button.js
@@ -10,63 +10,82 @@ modules.push({
 		function init(outputDOMElems){
 			outputDOMElems[0].innerHTML = "Button color:";
 			outputDOMElems[1].innerHTML = "Button text:";
-			outputDOMElems[2].innerHTML = "Light color:";
 		}
 
 		function onchange(data, outputDOMElems){
 			var buttonColor = getDataElemByName(data, "buttonColor").value;
 			var buttonText = getDataElemByName(data, "buttonText").value;
-			var lightColor = getDataElemByName(data, "lightColor").value;
 			var batteryCount = generalData.batteryCount;
+			var batteryCountIsSet = batteryCount !== "" && !isNaN(Number(batteryCount));
 			var indicators = generalData.indicators;
-			function holdRelease(){
-				if(buttonColor!=="" &&
-					buttonText!=="" &&
-					batteryCount!==""){
-					if(buttonColor==="blue" &&
-						buttonText==="Abort")
-						return "Hold";
-					else if(batteryCount>1 &&
-							buttonText==="Detonate")
+			var checkCAR = false;
+			var resCARon = "";
+			var checkFRK = false;
+			var resFRKon = "";
+			var resultElem = outputDOMElems[2];
+			var holdRelease = function(){
+				if(buttonColor === "")
+					return "Set the Button's color.";
+				if(buttonText === "") 
+					return "Set the Button's text.";
+				if(buttonColor==="blue" &&
+					buttonText==="Abort")
+					return "Hold";
+				if(buttonText==="Detonate"){
+					if(!batteryCountIsSet)
+						return "Set the battery count.";
+					else if(batteryCount > 1)
 						return "Release";
-					else if(buttonColor==="white" &&
-							hasLitIndicator("CAR"))
-						return "Hold";
-					else if(batteryCount>2 &&
-							hasLitIndicator("FRK"))
-						return "Release";
-					else if(buttonColor==="yellow")
-						return "Hold";
-					else if(buttonColor==="red" &&
-							buttonText==="Hold")
-						return "Release";
-					else
-						return "Hold";
 				}
-				else{
-					return "Not enough information";
+				if(buttonColor==="white"){
+					if(hasLitIndicator("CAR"))
+						return "Hold";
+					else{
+						checkCAR = true;
+						resCARon = "Hold";
+					}
 				}
-			}
+				if(!batteryCountIsSet)
+					return "Set the battery count.";
+				else {
+					if(batteryCount > 2){
+						if(hasLitIndicator("FRK"))
+							return "Release";
+						else{
+							checkFRK = true;
+							resFRKon = "Release";
+						}
+					}
+				}
+				if(buttonColor==="yellow")
+					return "Hold";
+				if(buttonColor==="red" &&
+						buttonText==="Hold")
+					return "Release";
+				else
+					return "Hold";
+			}();
 
-			if(holdRelease() === "Release"){
-				outputDOMElems[3].innerHTML = "Press and release immediately.";
+			var resText = "";
+			if(checkCAR && !checkFRK && resCARon !== holdRelease){
+				resText += "Check for lit <b>CAR</b> indicator. If not present or not lit:<br>";
+			} else if(!checkCAR && checkFRK && resFRKon !== holdRelease){
+				resText += "Check for lit <b>FRK</b> indicator. If not present or not lit:<br>";
+			} else if(checkCAR && checkFRK && (resCARon !== holdRelease || resFRKon !== holdRelease)){
+				resText += "Check for lit <b>CAR and FRK</b> indicators. If not present or not lit:<br>";
 			}
-			else if(holdRelease() === "Hold"){
-				if(lightColor!==""){
-					if(lightColor==="blue")
-						outputDOMElems[3].innerHTML = "Release when 4 is visible.";
-					else if(lightColor==="yellow")
-						outputDOMElems[3].innerHTML = "Release when 5 is visible.";
-					else
-						outputDOMElems[3].innerHTML = "Release when 1 is visible.";
-				}
-				else{
-					outputDOMElems[3].innerHTML = "No light color please input it.";
-				}
+			
+			if(holdRelease === "Release"){
+				resText += "<b>Press</b> and release immediately.";
+			}else if(holdRelease === "Hold"){
+				resText += "<b>Hold</b> down the button and check the appearing light:<br>";
+				resText += "<b>Blue</b>: release when <b>4</b> is visible.<br>";
+				resText += "<b>Yellow</b>: release when <b>5</b> is visible.<br>";
+				resText += "Anything else: release when <b>1</b> is visible.";
+			}else{
+				resText += holdRelease;
 			}
-			else{
-				outputDOMElems[3].innerHTML = "No serial last digit or battery count.";
-			}
+			resultElem.innerHTML = resText;
 
 			function hasLitIndicator(indicator){
 				for(var i=0;i<indicators.length;++i){
@@ -85,8 +104,6 @@ modules.push({
 				{func : "out", type : "text", name : "buttonTextLabel"},
 				{func : "in", type : "select", name : "buttonText", values : ["Abort","Detonate","Hold","[Other]"]},
 				{func : "layout", type : "lineBreak"},
-				{func : "out", type : "text", name : "lightColotLabel"},
-				{func : "in", type : "radioC", name : "lightColor", values : ["blue", "white", "yellow", "none"]},
 				{func : "out", type : "text", name : "outText"}
 			],
 			init : init,
