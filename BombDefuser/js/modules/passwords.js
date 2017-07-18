97790b4fcfe8d2dffc36848eab06138c596a2186
Bundas102 <robert.ban102@gmail.com>
2017-07-18 21:04:51 +0200

    passwords done

--- BombDefuser/js/modules/passwords.js
+++ BombDefuser/js/modules/passwords.js
@@ -13,8 +13,32 @@ modules.push({
 		}
 
 		function onchange(data, outputDOMElems){
-            
-            
+            var passwords = ["about","after","again","below","could",
+                             "every","first","found","great","house",
+                             "large","learn","never","other","place",
+                             "plant","point","right","small","sound",
+                             "spell","still","study","their","there",
+                             "these","thing","think","three","water",
+                             "where","which","world","would","write"];
+            var drums = [];
+            for(var i = 0; i < 5; i++){
+                drums.push(getDataElemByName(data,`letter${i+1}`).value);
+            }
+            var possiblePwds = [];
+            passwords.forEach(function(pwd){
+                if(possible(pwd))
+                    possiblePwds.push(pwd);
+            });
+            var res = possiblePwds.length > 9 ? "Too many results." : possiblePwds.join(", ");
+            outputDOMElems[1].innerHTML = res === "" ? "No matching pwd." : res;
+
+            function possible(pwd){
+                for(var i = 0; i < 5; i++){
+                    if(drums[i] !== "" && drums[i].indexOf(pwd[i]) == -1)
+                        return false;
+                }
+                return true;
+            }            
 		}
 
 		return {
